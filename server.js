a5122228696a40f2c4b5763d59cfd48c5dd43fe1
matiastucci <matiastucci@gmail.com>
2014-11-05 00:57:23 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var app   = require('express')();
+var http  = require('http').Server(app);
+var io    = require('socket.io')(http);
+
+var port  = process.env.PORT || 8080;
+
+app.get('/', function(req, res){
+  res.send('welcome to the socket-move server');
+});
+
+io.on('connection', function (socket) {
+
+  socket.on('tiltLR', function (data) {
+    io.emit('tiltLR', data);
+  });
+
+  socket.on('tiltFB', function (data) {
+    io.emit('tiltFB', data);
+  });
+
+  socket.on('dir', function (data) {
+    io.emit('dir', data);
+  });
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+});
